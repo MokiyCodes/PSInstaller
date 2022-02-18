7b5e1de9c43cde8a11eb47afca90484b0100aacc
Mokiy <92001109+MokiyCodes@users.noreply.github.com>
2022-02-18 22:00:29 +0100

    Improve cleaning up, fix branding

--- Installer.cs
+++ Installer.cs
@@ -23,6 +23,8 @@ class Installer
   private static string InstallLocation = Path.Combine(Environment.ExpandEnvironmentVariables("%ProgramFiles%"), "PSTools");
   private static string PSZipLocation = Path.Combine(InstallLocation, "PSToolsArchive.zip");
   private static string ZipSubdir = "PSTools-main";
+  private static string AppName = "PSTools";
+  private static string FullAppName = "PSTools";
   private static void die()
   {
     Process.GetCurrentProcess().Kill();
@@ -30,8 +32,8 @@ class Installer
   public static void Title()
   {
     Console.Clear();
-    Console.WriteLine(@"
-   PSTools Installer
+    Console.WriteLine($@"
+   {AppName} Installer
         0.1.0
 ");
   }
@@ -40,6 +42,8 @@ class Installer
     PSURL = "https://codeload.github.com/Mokiy2/SysInternalsSuite/zip/refs/heads/main";
     InstallLocation = Path.Combine(Environment.ExpandEnvironmentVariables("%ProgramFiles%"), "SysinternalsSuite");
     ZipSubdir = "SysInternalsSuite-main";
+    AppName = "SysInt.";
+    FullAppName = "SysInternals Suite";
     Console.WriteLine("Installing the full SysInternals Suite instead of just PSTools");
   }
   private static string[] args = Array.Empty<string>();
@@ -49,6 +53,7 @@ class Installer
     {
       if (!args.Contains("force"))
       {
+        Title();
         Console.Title = "Existing Installation Found!";
         Console.Write("Existing installation found at " + InstallLocation + ".\nNot removing this will cause the installation to abort.\nDo you wish to remove this? [y/N] ");
         string? input = Console.ReadLine();
@@ -78,11 +83,11 @@ class Installer
       ProcessStartInfo startInfo = new(exeName);
       startInfo.UseShellExecute = true;
       startInfo.Verb = "runas";
-      startInfo.Arguments = "rra " + String.Join(" ",args).ToLower();
+      startInfo.Arguments = "rra " + String.Join(" ", args).ToLower() + (AppName == "SysInt." ? "full-suite" : "");
       Process? proc = Process.Start(startInfo);
       if (proc != null)
       {
-        Log("Waiting for administrator process to exit...\nPlease complete installation in other process...","Waiting for other process");
+        Log("Waiting for administrator process to exit...\nPlease complete installation in other process...", "Waiting for other process");
         proc.WaitForExit();
       }
       die();
@@ -149,11 +154,11 @@ class Installer
     if (!args.Contains("force") && !args.Contains("rra"))
     {
       Log("This program is not created by, nor endorsed by Microsoft.\nPressing s will install the whole SysInternals Suite, pressing any other key will only install PSUtils.\nPress any key to start...", "Waiting for key to be pressed...");
-      if (Console.ReadKey().Key.ToString().ToLower()=="s") UseFullSysInternalsSuite();
+      if (Console.ReadKey().Key.ToString().ToLower() == "s") UseFullSysInternalsSuite();
     };
     if (!OperatingSystem.IsOSPlatform("windows"))
     {
-      Log("PSTI (PowerShell Tools Installer) only works on Windows!\nPlease use your platform's alternatives to PSTools instead.","Unsupported OS");
+      Log("PSTI (PowerShell Tools Installer) only works on Windows!\nPlease use your platform's alternatives to PSTools instead.", "Unsupported OS");
       return;
     }
     if (!OperatingSystem.IsOSPlatformVersionAtLeast("Windows", 4))
@@ -165,12 +170,12 @@ class Installer
       Console.Error.WriteLine("[WARN] Not running Windows 7 or above! Bugs are almost guaranteed to occur.");
     }
     Title();
-    Log("Checking Pemrissions and elevating if necessary...","Checking Permissions");
+    Log("Checking Pemrissions and elevating if necessary...", "Checking Permissions");
     if (Elevate())
     {
       Log("Running at correct permission level");
       EnsureDir(); // Ensure directory exists and is empty
-      Log("Downloading PSTools from "+PSURL, "Downloading PSTools");
+      Log($"Downloading {FullAppName} from " + PSURL, $"Downloading {FullAppName}");
 
       /*
       HttpResponseMessage downloadResponse = await httpClient.GetAsync(PSURL); // "https://via.placeholder.com/300.png"
@@ -182,40 +187,47 @@ class Installer
       new WebClient().DownloadFile(PSURL, PSZipLocation);
 #pragma warning restore SYSLIB0014 // Type or member is obsolete
 
-      Log("Extracting PSTools", "Extracting PSTools");
+      Log($"Extracting {FullAppName}");
       ZipFile.ExtractToDirectory(PSZipLocation, InstallLocation);
+      Log("Copying Files to Installation Directory", "Copying Files");
       CopyAll(new DirectoryInfo(Path.Combine(InstallLocation, ZipSubdir)), new DirectoryInfo(InstallLocation));
 
-      Directory.Delete(Path.Combine(InstallLocation, ZipSubdir), true);
-
       File.Delete(Path.Combine(InstallLocation, "bin", "README.md"));
-      File.WriteAllText(Path.Combine(InstallLocation,"README.txt"),$@"
-/// PSTools | Installed from {PSURL} by PSTI (PowerShell Tools Installer) ///
-By using PSTools you agree to the Windows Sysinternals EULA. It can be found in bin/EULA.txt or at https://docs.microsoft.com/en-us/sysinternals/license-terms");
+      File.WriteAllText(Path.Combine(InstallLocation, "README.txt"), $@"
+/// {FullAppName} | Installed from {PSURL} by PSTI (PowerShell Tools Installer) ///
+By using {FullAppName} you agree to the Windows Sysinternals EULA. It can be found in bin/EULA.txt or at https://docs.microsoft.com/en-us/sysinternals/license-terms");
 
       if (!Directory.Exists(Path.Combine(InstallLocation, "bin"))) throw new Exception("Cannot find binary files in extracted archive");
 
-      Log("Adding to PATH", "Adding to PATH");
+      Log("Adding to PATH");
       string EnvVariableName = "PATH";
       EnvironmentVariableTarget scope = EnvironmentVariableTarget.Machine; // or User
       string? oldValue = Environment.GetEnvironmentVariable(EnvVariableName, scope);
-      string? newValue = oldValue + @";"+Path.Combine(InstallLocation, "bin");
+      string? newValue = oldValue + @";" + Path.Combine(InstallLocation, "bin");
       Environment.SetEnvironmentVariable(EnvVariableName, newValue, scope);
+
+      Log("Cleaning Up");
+      Directory.Delete(Path.Combine(InstallLocation, ZipSubdir), true);
+      File.Delete(PSZipLocation);
 
       if (!args.Contains("accepteula"))
       {
-        Log("By using SysInternals, you agree to the SysInternals EULA found at https://docs.microsoft.com/en-us/sysinternals/license-terms \nBy default, Microsoft asks you to accept these every time you run any sysinternals application." + @"
+        Title();
+        Log($@"{FullAppName} is now installed! However, we need to ask you one thing...
+
+By using {FullAppName}, you agree to the SysInternals EULA found at https://docs.microsoft.com/en-us/sysinternals/license-terms
+By default, Microsoft asks you to accept these every time you run any SysInternals application." + @"
 If you accept the SysInternals EULA and press Y here, we will remove said prompt.", "EULA Notice");
         Console.Write("Do you agree to the SysInternals EULA (End-users License Agreement)? [y/N] ");
         ConsoleKeyInfo? Input = Console.ReadKey();
         if (Input == null || Input.Value.Key.ToString().ToLower() != "y") return;
       };
       Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("Software\\Sysinternals");
-      key.SetValue("EulaAccepted",1);
+      key.SetValue("EulaAccepted", 1);
       Log(@"
 Finished Installation!
 
-~ Mokiy#0001","Done!");
+~ Mokiy#0001", "Done!");
     }
   }
   public static void Main(string[] _args)
